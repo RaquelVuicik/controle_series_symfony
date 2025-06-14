819e546292bc677be89fcfb78f2553daa855ac83
RaquelVuicik <raquelvuicik1@gmail.com>
2025-06-14 18:59:56 -0300

    definindo a primeira rota e usando request e response

---
+++ src/Controller/OlaMundoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OlaMundoController
+{
+    #[Route('/ola_mundo')]
+    public function index(Request $request): Response
+    {
+        return new Response(
+            '<h1>ID:</h1>' . $request->query->get('id'),
+            401,
+            [
+                'X-Qualquer-Coisa' => 'Valor'
+            ]
+        );
+    }
+}
